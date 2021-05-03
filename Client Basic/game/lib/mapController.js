08cefbb19d4f5ef589f15e3835faa42432fd96a4
Alex <Wilmer.DelgadoAlama09@gmail.com>
2021-05-03 04:37:00 +0200

    re-add maps

--- Client Basic/game/lib/mapController.js
+++ Client Basic/game/lib/mapController.js
@@ -1,22 +1,280 @@
 var cls = require('./class');
 var Logger = require('./logger');
 var Map = require('./map');
+
 module.exports = MapController = cls.Class.extend({
     init: function (load) {
         var self = this;
         self.maps = {};
+
         if (load) {
-            let mapID = 0;
-            let mapCount = 44;
-            for(mapID; mapID < mapCount; mapID++){
-                let map = new Map(mapID);
-                map.loadMap(function(){
-                    map.LoadPoints();
-                    self.maps[mapID] = map;
-                });
-            }
+            var nmap0 = new Map(0);
+            nmap0.loadMap(function () {
+                nmap0.LoadPoints();
+                self.maps[0] = nmap0;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap1 = new Map(0);
+            nmap1.loadMap(function () {
+                nmap1.LoadPoints();
+                self.maps[0] = nmap1;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap2 = new Map(0);
+            nmap2.loadMap(function () {
+                nmap2.LoadPoints();
+                self.maps[0] = nmap2;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap3 = new Map(0);
+            nmap3.loadMap(function () {
+                nmap3.LoadPoints();
+                self.maps[0] = nmap3;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap4 = new Map(0);
+            nmap4.loadMap(function () {
+                nmap4.LoadPoints();
+                self.maps[0] = nmap4;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap5 = new Map(0);
+            nmap5.loadMap(function () {
+                nmap5.LoadPoints();
+                self.maps[0] = nmap5;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap6 = new Map(0);
+            nmap6.loadMap(function () {
+                nmap6.LoadPoints();
+                self.maps[0] = nmap6;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap7 = new Map(0);
+            nmap7.loadMap(function () {
+                nmap7.LoadPoints();
+                self.maps[0] = nmap7;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap8 = new Map(0);
+            nmap8.loadMap(function () {
+                nmap8.LoadPoints();
+                self.maps[0] = nmap8;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap9 = new Map(0);
+            nmap9.loadMap(function () {
+                nmap9.LoadPoints();
+                self.maps[0] = nmap9;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap10 = new Map(0);
+            nmap10.loadMap(function () {
+                nmap10.LoadPoints();
+                self.maps[0] = nmap10;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap11 = new Map(0);
+            nmap11.loadMap(function () {
+                nmap11.LoadPoints();
+                self.maps[0] = nmap11;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap12 = new Map(0);
+            nmap12.loadMap(function () {
+                nmap12.LoadPoints();
+                self.maps[0] = nmap12;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap13 = new Map(0);
+            nmap13.loadMap(function () {
+                nmap13.LoadPoints();
+                self.maps[0] = nmap13;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap14 = new Map(0);
+            nmap14.loadMap(function () {
+                nmap14.LoadPoints();
+                self.maps[0] = nmap14;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap15 = new Map(0);
+            nmap15.loadMap(function () {
+                nmap15.LoadPoints();
+                self.maps[0] = nmap15;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap16 = new Map(0);
+            nmap16.loadMap(function () {
+                nmap16.LoadPoints();
+                self.maps[0] = nmap16;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap17 = new Map(0);
+            nmap17.loadMap(function () {
+                nmap17.LoadPoints();
+                self.maps[0] = nmap17;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap18 = new Map(0);
+            nmap18.loadMap(function () {
+                nmap18.LoadPoints();
+                self.maps[0] = nmap18;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap19 = new Map(0);
+            nmap19.loadMap(function () {
+                nmap19.LoadPoints();
+                self.maps[0] = nmap19;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap20 = new Map(0);
+            nmap20.loadMap(function () {
+                nmap20.LoadPoints();
+                self.maps[0] = nmap20;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap21 = new Map(0);
+            nmap21.loadMap(function () {
+                nmap21.LoadPoints();
+                self.maps[0] = nmap21;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap22 = new Map(0);
+            nmap22.loadMap(function () {
+                nmap22.LoadPoints();
+                self.maps[0] = nmap22;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap23 = new Map(0);
+            nmap23.loadMap(function () {
+                nmap23.LoadPoints();
+                self.maps[0] = nmap23;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap24 = new Map(0);
+            nmap24.loadMap(function () {
+                nmap24.LoadPoints();
+                self.maps[0] = nmap24;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap25 = new Map(0);
+            nmap25.loadMap(function () {
+                nmap25.LoadPoints();
+                self.maps[0] = nmap25;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap26 = new Map(0);
+            nmap26.loadMap(function () {
+                nmap26.LoadPoints();
+                self.maps[0] = nmap26;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap27 = new Map(0);
+            nmap27.loadMap(function () {
+                nmap27.LoadPoints();
+                self.maps[0] = nmap27;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap28 = new Map(0);
+            nmap28.loadMap(function () {
+                nmap28.LoadPoints();
+                self.maps[0] = nmap28;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap29 = new Map(0);
+            nmap29.loadMap(function () {
+                nmap29.LoadPoints();
+                self.maps[0] = nmap29;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap30 = new Map(0);
+            nmap30.loadMap(function () {
+                nmap30.LoadPoints();
+                self.maps[0] = nmap30;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap31 = new Map(0);
+            nmap31.loadMap(function () {
+                nmap31.LoadPoints();
+                self.maps[0] = nmap31;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap32 = new Map(0);
+            nmap32.loadMap(function () {
+                nmap32.LoadPoints();
+                self.maps[0] = nmap32;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap33 = new Map(0);
+            nmap33.loadMap(function () {
+                nmap33.LoadPoints();
+                self.maps[0] = nmap33;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap34 = new Map(0);
+            nmap34.loadMap(function () {
+                nmap34.LoadPoints();
+                self.maps[0] = nmap34;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap35 = new Map(0);
+            nmap35.loadMap(function () {
+                nmap35.LoadPoints();
+                self.maps[0] = nmap35;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap36 = new Map(0);
+            nmap36.loadMap(function () {
+                nmap36.LoadPoints();
+                self.maps[0] = nmap36;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap37 = new Map(0);
+            nmap37.loadMap(function () {
+                nmap37.LoadPoints();
+                self.maps[0] = nmap37;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap38 = new Map(0);
+            nmap38.loadMap(function () {
+                nmap38.LoadPoints();
+                self.maps[0] = nmap38;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap39 = new Map(0);
+            nmap39.loadMap(function () {
+                nmap39.LoadPoints();
+                self.maps[0] = nmap39;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap40 = new Map(0);
+            nmap40.loadMap(function () {
+                nmap40.LoadPoints();
+                self.maps[0] = nmap40;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap41 = new Map(0);
+            nmap41.loadMap(function () {
+                nmap41.LoadPoints();
+                self.maps[0] = nmap41;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap42 = new Map(0);
+            nmap42.loadMap(function () {
+                nmap42.LoadPoints();
+                self.maps[0] = nmap42;
+                Logger.debug('MapLoad: ' + 0);
+            });
+            var nmap43 = new Map(0);
+            nmap43.loadMap(function () {
+                nmap43.LoadPoints();
+                self.maps[0] = nmap43;
+                Logger.debug('MapLoad: ' + 0);
+            });
         }
     },
+
     arrayClone: function (arr) {
         var nm = [];
         for (var i = 0; i < arr.length; i++) {
