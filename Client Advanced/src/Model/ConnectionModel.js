f53d73fd2f82c2e44dca0c54cd52ff0c9d8f6665
Alex <wilmer.delgadoalama09@gmail.com>
2021-08-13 18:19:05 -0500

    clean project and add dotenv config

---
+++ Client Advanced/src/Model/ConnectionModel.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+let mysql = require("mysql2/promise");
+var Logger = require("../game/lib/logger");
+
+module.exports = class Connection {
+  constructor() {
+    var self = this;
+    this.connection = null;
+    this.host = process.env.DB_HOST;
+    this.user = process.env.DB_USER;
+    this.password = process.env.DB_PASSWORD;
+    this.database = process.env.DB_DATABASE;
+    this.connection = mysql.createPool({
+      connectionLimit: process.env.DB_CONNECTION_LIMIT,
+      host: self.host,
+      user: self.user,
+      password: self.password,
+      database: self.database,
+    });
+    if (this.connection !== null) {
+      Logger.normal("MYSQL Connect!");
+    }
+  }
+};
