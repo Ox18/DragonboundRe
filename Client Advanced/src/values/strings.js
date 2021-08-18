30454710f6702debfe32f7061f887d530aa66e1e
Wilmer Delgado <Wilmer.DelgadoAlama09@gmail.com>
2021-08-18 15:27:15 -0500

    Update strings.js

--- Client Advanced/src/values/strings.js
+++ Client Advanced/src/values/strings.js
@@ -1,3 +1,7 @@
 module.exports = {
+  // un buen ejemplo para aplicar stringParam del helper
+  // const text = strings.server_gm_gift_text;
+  // const params = ["Alex", "Easter 2017 (RARE)", "Flag", "Safed"];
+  // el resultado es: "Alex sent gift -> Ash (Rare) [Head] to -> Safed"
   server_gm_gift_text: "$? sent gift -> $? [$?] to -> $?",
 }
