7c2b115300d45686796bcc9b4af0345f1284a380
Alex <Wilmer.DelgadoAlama09@gmail.com>
2022-02-03 14:39:20 -0500

    add comments to library

--- src/core/Lib/Model.lib.js
+++ src/core/Lib/Model.lib.js
@@ -3,11 +3,34 @@ class ModelLib{
         Object.assign(this, ...args);
     }
 
+    /**
+     * Method to get the properties of the model
+     * @returns {Array}
+     * @example
+     * const model = {
+     *   name: "John",
+     *   age: 30
+     * };
+     * const properties = model._getProperties();
+     * console.log(properties); // ["name", "age"]
+     */
     _getProperties(){
         return Object.keys(this);
     }
 
-    static fromObject(obj){
+    /**
+     * Method to instantiate a model from a hashmap
+     * @param {Object} obj Object to be compared
+     * @returns Instance of ModelLib
+     * @example
+     * const model = {
+     *  name: "John",
+     *  age: 30
+     * };
+     * const modelLib = ModelLib.fromHashMap(model);
+     * console.log(modelLib.name); // John
+     */
+    static fromHashMap(obj){
         let model = new this();
         model._getProperties().forEach(prop => {
             if(obj[prop]){
@@ -16,13 +39,25 @@ class ModelLib{
         });
         return model; 
     }
+
+    /**
+     * Method to get object properties but id property is excluded
+     * @returns {Object}
+     */
+    toHashMapToDatabase(){
+        let hashMap = this.toHashMap();
+        delete hashMap.id;
+        return hashMap;
+    }
     
+    /**
+     * Method to get object properties
+     * @returns {Object}
+     */
     toHashMap(){
         let hashMap = {};
         this._getProperties().forEach(prop => {
-            if(prop !== "id"){
-                hashMap[prop] = this[prop];
-            }
+            hashMap[prop] = this[prop];
         });
         return hashMap;
     }
