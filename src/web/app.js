ac3ae709298c5200bad6ad0c2a46d5305f4443bb
Alex <Wilmer.DelgadoAlama09@gmail.com>
2022-02-03 09:55:40 -0500

    change structure

--- src/web/app.js
+++ src/web/app.js
@@ -1,24 +1,23 @@
-var express = require('express');
+import express from 'express';
 import "express-async-errors";
-var cors = require('cors');
+import cors from 'cors';
+
+import routes from "./routes";
 
 const app = express();
 
-app.use(express.json());
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
-app.use(cors({ origin: true }));
-
 app.set('port', process.env.PORT || 3000);
+app.use(express.json());
+app.use(routes);
+app.use(cors({ origin: true }));
 
 app.use((err, req, res, next) => {
     return res.status(500).json({
         status: "error",
-        message: "Internal server error: " + err.message,
+        message: "Internal server error: " + err.message
     });
 });
 
 app.listen(app.get('port'), () => {
-    console.log('Server started on port ' + app.get('port'));
+    console.log(`Server listening on port ${app.get('port')}`);
 });
