b69bf005fba43ec69ac67208d85047b27e636f59
Alex <Wilmer.DelgadoAlama09@gmail.com>
2022-02-03 09:29:32 -0500

    remove all file

--- Client Advanced/src/library/stacks/index.js
+++
@@ -1,5 +0,0 @@
-const FIFO = require("./FIFO");
-
-module.exports = {
-  FIFO
-};
--- Client Advanced/src/types/ServerTypes.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    SERVER_NORMAL   : 0,
-    SERVER_MATCH    : 1,
-    SERVER_PRIX     : 2
-};
