a0b43de4ea0714104b26b7ba43d832ac65d1d00e
Alex <Wilmer.DelgadoAlama09@gmail.com>
2022-02-04 09:56:46 -0500

    add consts

--- src/web/controllers/w2.js
+++ src/web/controllers/w2.js
@@ -1,20 +1,19 @@
 import ServerService from "../../core/Service/ServerService";
+import { NUMBER_CONST } from "../../consts/NumberConst";
 
 const serverService = new ServerService();
 
 export const get = async (req, res) => {
     let servers = await serverService.findAll();
         servers = servers.map(server => [server.name, server.type, server.port, server.players, server.maxPlayers, server.minRank, server.maxRank]);
-    const SERVER_VERSION = 133;
-    const SIZE_FILE = 78212;
-    const SIZE_FILE_CHUNK = 7961;
+    
     const DATE_NOW = new Date();
     
     res.json([
-        SERVER_VERSION,
-        SIZE_FILE,
-        SIZE_FILE_CHUNK,
+        NUMBER_CONST.SERVER.VERSION,
+        NUMBER_CONST.FILE.SIZE,
+        NUMBER_CONST.FILE.CHUNK,
         ...servers,
         DATE_NOW
-    ])
+    ]);
 }
