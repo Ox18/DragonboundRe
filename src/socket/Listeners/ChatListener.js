69d16544a1f90962ac9d548b2832fec9779dbd80
Alex <Wilmer.DelgadoAlama09@gmail.com>
2022-02-11 11:48:11 -0500

    test chat received

--- src/socket/Listeners/ChatListener.js
+++ src/socket/Listeners/ChatListener.js
@@ -22,7 +22,10 @@ class ChatListener extends ModelLib{
         const guild_name = "Dev";
         const chat_type = CHAT_TYPE.GM;
         const rank = 26;
-        this.account.send([0, message, game_id, chat_type, rank, guild_name, 6, 7, 8])
+        const dado_1 = 0;
+        const dado_2 = 0;
+        const dado_3 = 0;
+        this.account.send([0, message, game_id, chat_type, rank, guild_name, dado_1, dado_2, dado_3]);
     }
 }
 
