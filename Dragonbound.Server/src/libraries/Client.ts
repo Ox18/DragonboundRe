08b2cee7cb4ca387307c73d136ed82b99e3ce61c
Alex <Wilmer.DelgadoAlama09@gmail.com>
2022-02-25 15:50:43 -0500

    add listeners to client

--- Dragonbound.Server/src/libraries/Client.ts
+++ Dragonbound.Server/src/libraries/Client.ts
@@ -7,14 +7,26 @@ class Client{
         public contextServer: Server
     ){
         connection.on("message", this.onMessage.bind(this));
+        connection.on("close", this.onClose.bind(this));
+        connection.on("error", this.onError.bind(this));
+        connection.on("open", this.onOpen.bind(this));
+
     }
 
     onMessage(message: any){
         const data: Array<String | Number> = Packet.DecodeBuffer(message);
-        console.log(data);
-        // const buffer = message.toJSON();
-        // const data = Packet.BufferArrayToNormalArray(buffer.data);
-        // console.log(data);
+    }
+
+    onClose(...args: any){
+        console.log(args);
+    }
+
+    onError(){
+        throw new Error("OnError is not implemented");
+    }
+
+    onOpen(){
+        throw new Error("OnOpen is not implemented");
     }
 }
 
