8939f1ec3bcfd2c963bcfcbce5f530ac4db518d4
Alex <Wilmer.DelgadoAlama09@gmail.com>
2022-02-28 10:29:50 -0500

    The js was separated from the web client to study it and make configurations quickly and easily

--- Dragonbound.Client/gulpfile.js
+++ Dragonbound.Client/gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require("gulp");
 var minify = require("gulp-minify");
 var fs = require('fs');
+var concat = require('gulp-concat');
 
 function readModuleFile(path, callback) {
   try {
@@ -11,18 +12,29 @@ function readModuleFile(path, callback) {
   }
 }
 
+gulp.task("merges", ()=>{
+  gulp.src([
+    "src/web/public/js/jquery/jq_header.js",
+    "src/web/public/js/dragonbound.js",
+    "src/web/public/js/jquery/jq_footer.js"
+  ])
+  .pipe(concat("dragonbound-merge.js"))
+  .pipe(gulp.dest("src/web/public/js"))
+});
+
 gulp.task("minify-app", function(){
-  gulp.src("src/web/public/js/main.js", { allowEmpty: true })
+  gulp.src("src/web/public/js/dragonbound-merge.js", { allowEmpty: true })
   .pipe(minify({ noSource: true }))
   .pipe(gulp.dest("./src/web/public/js"));
 });
 
 gulp.task("minify-app-to-serialized", function(){
-  readModuleFile('./src/web/public/js/main-min.js', function (err, words) {
+  readModuleFile('./src/web/public/js/dragonbound-merge-min.js', function (err, words) {
     const data = JSON.stringify(words, null, 2)
     fs.writeFile('./src/web/public/js/1089276.js', data, function (err) {
       if (err) return console.log(err);
     });
+    fs.unlinkSync('./src/web/public/js/dragonbound-merge-min.js')
   });
 })
 
