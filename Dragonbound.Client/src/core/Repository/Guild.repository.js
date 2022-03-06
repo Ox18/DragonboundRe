9c01cd50e5a63d553180a96571de4b6420c249f3
Alex <Wilmer.DelgadoAlama09@gmail.com>
2022-03-05 20:15:46 -0500

    add test to guild repository

--- Dragonbound.Client/src/core/Repository/Guild.repository.js
+++ Dragonbound.Client/src/core/Repository/Guild.repository.js
@@ -1,7 +1,17 @@
 import GuildData from "../Network/data/Guild.data";
+import Guild from "../Model/Guild";
 
 class GuildRepository{
-
+    findById(id){
+        return new Promise((resolve, reject)=>{
+            const guild = GuildData.find(guild => guild.id === id);
+            if(guild){
+                resolve(Guild.fromHashMap(guild));
+            }else{
+                reject(new Error("Guild not found"));
+            }
+        });
+    }
 }
 
 export default GuildRepository;
