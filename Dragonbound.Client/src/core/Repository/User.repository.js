e0920dc823724b3194e280025316d46ddbad6d9b
Alex <Wilmer.DelgadoAlama09@gmail.com>
2022-03-05 20:26:26 -0500

    add test to user repo

--- Dragonbound.Client/src/core/Repository/User.repository.js
+++ Dragonbound.Client/src/core/Repository/User.repository.js
@@ -3,7 +3,19 @@ import User from "../Model/User";
 
 class UserRepository{
     findByAccountId(account_id){
-        return User.fromHashMap(UserData.find(user => user.account_id === account_id));
+        return new Promise((resolve, reject)=>{
+            try{
+                const user = UserData.find(user => user.account_id === account_id);
+                if(user){
+                    resolve(User.fromHashMap(user));
+                }
+                else{
+                    reject(new Error("User not found"));
+                }
+            }catch(e){
+                reject(e);
+            }
+        });
     }
 }
 
