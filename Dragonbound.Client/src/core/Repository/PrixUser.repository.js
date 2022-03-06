ccda59f4ec38cf979b4d4a2309421ec79c463017
Alex <Wilmer.DelgadoAlama09@gmail.com>
2022-03-05 20:20:20 -0500

    add test to prix user repository

--- Dragonbound.Client/src/core/Repository/PrixUser.repository.js
+++ Dragonbound.Client/src/core/Repository/PrixUser.repository.js
@@ -1,7 +1,18 @@
 import PrixUserData from "../Network/data/PrixUser.data";
+import PrixUser from "../Model/PrixUser";
 
 class PrixUserRepository{
-
+    findByUserId(userId){
+        return new Promise((resolve, reject)=>{
+            const prixUser = PrixUserData.find(prixUser => prixUser.user_id === userId);
+            if(prixUser){
+                resolve(PrixUser.fromHashMap(prixUser));
+            }
+            else{
+                reject(new Error("PrixUser not found"));
+            }
+        })
+    }
 }
 
 export default PrixUserRepository;
