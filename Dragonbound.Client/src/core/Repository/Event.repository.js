a4e5533724ba42c1be5aedc7983cccfd393d05e9
Alex <Wilmer.DelgadoAlama09@gmail.com>
2022-03-05 20:13:33 -0500

    add test to event repository

--- Dragonbound.Client/src/core/Repository/Event.repository.js
+++ Dragonbound.Client/src/core/Repository/Event.repository.js
@@ -1,5 +1,16 @@
 import EventData from "../Network/data/EventData";
 
-class EventRepository{}
+class EventRepository{
+    findByUserId(userId){
+        return new Promise((resolve, reject)=>{
+            const event = EventData.find(event => event.user_id === userId);
+            if(event){
+                resolve(event);
+            }else{
+                reject(new Error("Event not found"));
+            }
+        });
+    }
+}
 
 export default EventRepository;
