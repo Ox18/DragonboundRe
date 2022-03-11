36ab71dc5ca8facb66c5dc76b83557e9e52918e8
Alex <Wilmer.DelgadoAlama09@gmail.com>
2022-03-11 10:27:46 -0500

    add test to resource not exist

--- Dragonbound.Client/src/core/Repository/Server.repository.spec.js
+++ Dragonbound.Client/src/core/Repository/Server.repository.spec.js
@@ -1,5 +1,6 @@
 import ServerRepository from "./Server.repository";
 import ParamNotValidException from "../Exception/ParamNotValidException";
+import ResourceNotFoundException from "../Exception/ResourceNotFoundException";
 
 const repo = new ServerRepository();
 
@@ -7,4 +8,9 @@ describe("Test Server Repository", ()=>{
     it("findById is not valid id", async ()=>{
        await expect(repo.findById(0)).rejects.toThrow(ParamNotValidException);
     });
+
+    it("Resource not exist", async ()=>{
+        const serverId = 3;
+        await expect(repo.findById(serverId)).rejects.toThrow(ResourceNotFoundException);
+    })
 })
