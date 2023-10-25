a853572b0be23dc98e30907faa82861b972c16c4
Ox18 <Wilmer.DelgadoAlama09@gmail.com>
2023-10-25 17:48:57 -0500

    fix: change port deploy

--- src/lib/application.ts
+++ src/lib/application.ts
@@ -6,8 +6,10 @@ export const application = async (dir: string): Promise<void> => {
   const config = configHealth();
 
   const controllers = await loadControllers(dir);
+
+  const port = process.env.PORT || config.service.port;
   
-  const framework = new Framework(config.service.port);
+  const framework = new Framework(port as number);
   framework.initialize(dir);
   framework.loadControllers(controllers);
   framework.start();
